44489d8199c308c8bb70cd8dd5bb7a8f65335732
Nazmul Hasan <nazmul.sbpgc@gmail.com>
2018-01-15 15:28:45 +0600

    Update DateConversion.java

--- DateTime/DateConversion.java
+++ DateTime/DateConversion.java
@@ -1,4 +1,6 @@
 import java.text.*;
+import java.util.*;
+import java.time.*;
 
 public class UtilToSqlDateConversion{
 	public static void main(String[] args){
@@ -9,5 +11,13 @@ public class UtilToSqlDateConversion{
 		// Formate a util date to any format
 		DateFormat df = new SimpleDateFormat("dd-MM-YYYY");
 		System.out.println("dateFormated date is : " + df.format(utilDate));
+		LocalDate ldate = LocalDate.now();
+		System.out.println("Local Date is: " + ldate);
+		// Creates a date from values
+		System.out.println("Another Local Date is: " + LocalDate.of(2017,12,15));
+		// Create a date form string
+		System.out.println("Date from string : " + LocalDate.parse("2018-01-15"));
+		// creates a date from zone
+		System.out.println("Another local date from zone: " + LocalDate.now(ZoneId.systemDefault()));
 	}
 }
